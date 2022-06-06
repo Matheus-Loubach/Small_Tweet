c7c3bdca6bf9cea494923ac9d1efacd7221609e1
Matheus Affonso <matheus.loubach@gmail.com>
2022-06-06 18:44:24 -0300

    start project

--- api/routes.js
+++ api/routes.js
@@ -1,8 +1,145 @@
 import Router from '@koa/router'
+import { PrismaClient } from '@prisma/client'
+import { CssSyntaxError } from 'postcss'
+import bcrypt from  'bcrypt'
+import  jwt  from 'jsonwebtoken'
   
 export const router = new Router()
 
-router.get('/tweets', function(ctx){
+
+const prisma = new PrismaClient()
+
+//bd recebe os dados
+router.get('/tweets', async function(ctx){
+  const [, token] = ctx.request.headers?.authorization?.split(' ') || []
+  console.log(token)
+  if(!token){
+      ctx.status = 401
+      return
+    }
+ try{    
+
+  const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+  const tweets = await prisma.tweet.findMany()
+  ctx.body = tweets
+  }catch(error){
+    ctx.status = 401
+    return
+  }
+})
+
+
+//enviar tt
+router.post('/tweets', async function(ctx){
+  const [, token] = ctx.request.headers?.authorization?.split(' ') || []
+  console.log(token)
+  if(!token){
+      ctx.status = 401
+      return
+    }
+ try{    
+
+  const payload = jwt.verify(token, process.env.JWT_SECRET)
+
+    const tweet = await prisma.tweet.create({
+      data: {
+        userId: payload.sub,
+        text: ctx.request.body.text
+      }
+    })
   
-  ctx.body = 'tweets'
+   ctx.body = tweet
+  }catch(error){
+    ctx.status = 401
+    return
+  }
+
+})
+//cadastro
+router.post('/signup', async function(ctx)
+{
+  const saltRounds = 10
+  const password = bcrypt.hashSync(ctx.request.body.password, saltRounds);
+
+  try{
+
+    const user = await prisma.user.create({
+ 
+      data:{
+        name: ctx.request.body.name,
+        username: ctx.request.body.username,
+        email: ctx.request.body.email,
+        password
+      }
+    })
+
+    const acessToken = jwt.sign({
+      sub: user.id
+    }, process.env.JWT_SECRET, { expiresIn: '24h'})
+
+    ctx.body = {
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      acessToken
+    }
+    
+  }catch(error){
+    if(error.meta && !error.meta.target)
+    {
+    console.log('Cadastro já existe')
+    ctx.body = "Email ou Usúario já existe"
+    ctx.status = 422
+    return
+    }
+  }
+})
+
+//LOGIN
+router.get('/login', async function(ctx){
+  const [, token] = ctx.request.headers.authorization.split(' ')
+  const [email, plainTextPassword] = Buffer.from(token, 'base64').toString().split(':')
+
+
+  //passa password//se achar o user
+  const user = await prisma.user.findUnique({
+    where:{ email }
+  })
+  
+
+  if(!user){
+    ctx.status = 404
+    return
+  }
+
+try{
+  //verifica a senha
+  const passwordMatch = bcrypt.compareSync(plainTextPassword, user.password)
+
+  //retorna user
+  if(passwordMatch){
+    const acessToken = jwt.sign({
+      sub: user.id
+    }, process.env.JWT_SECRET, { expiresIn: '24h'})
+  
+    ctx.body = {
+      id: user.id,
+      name: user.name,
+      username: user.username,
+      email: user.email,
+      acessToken
+    }
+  }
+}catch(error)
+{  if(error.meta && !error.meta.target)
+  {
+  ctx.status = 404
+  console.log('senha errada')
+  return
+  }
+}
+ 
+   
 })
---
+++ web/src/Home/index.jsx
@@ -0,0 +1,90 @@
+import { HeartIcon } from '@heroIcons/react/outline'
+import { useState } from 'react'
+
+//maximo de digitos 
+const MAX_TWEET_CHAR = 250
+
+function TweetForm(){
+  const [text, setText] = useState('')
+
+  function changeText(e){
+    setText(e.target.value)
+  }
+  //console.log(text)
+  return(
+
+    <div className='border-b border-silver p-4 space-y-6'>
+      <div className='flex space-x-5'>
+        <img src= '/src/Mask group.png' className='w-7'/>
+        <h1 className='font-bold text-xl'>Página inicial</h1>
+      </div>
+
+      <form className='pl-12 text-lg flex flex-col'>
+        <textarea
+        name='text'
+        value={text}
+        placeholder='O que está acontecendo?'
+        className='bg-transparent outline-none disabled:opacity-50'
+        onChange={changeText}
+        // disabled={text.length > MAX_TWEET_CHAR}
+        />        
+        <div className='flex justify-end items-center space-x-3'>
+        <span className='text-sm'>
+          <span>{text.length}/</span><span className='text-birdblue'>{MAX_TWEET_CHAR}</span>
+        </span>
+        <button
+         className='bg-birdblue px-5 py-2 rounded-full disabled:opacity-50'
+         disabled={text.length > MAX_TWEET_CHAR}
+         >Tweet
+         </button>
+        </div>
+      </form>
+
+    </div>
+  )
+}
+function Tweet({name, username, avatar, children}){
+  return(
+    <div className="flex space-x-3 p-4 border-b border-solid border-silver">
+    <div> 
+      <img src={avatar}/>
+      </div>
+      <div className='space-y-1'>
+        <span className="hover:text-birdblue font-bold text-sm">
+          {name}
+        </span>{' '}
+
+        <span className="text-sm text-silver">
+          @{username}
+        </span>
+
+        <p>{children}</p> 
+
+        <div className='flex space-x-1 text-silver text-sm items-center'>
+        <HeartIcon className='w-4 stroke-1 stroke-silver'/>
+        <span>1.5k</span>
+        </div>
+      </div>
+  </div>
+  )
+}
+
+export function Home(){
+  return(/*dinamico*/
+  <>
+  <TweetForm />
+  
+  <div>
+    <Tweet name="Elon Musk" username="elonMusk" avatar="/src/Mask group.png">
+    Let's make twitter maximu fun
+    </Tweet>
+
+    <Tweet name="Matheus Affonso" username="loubach" avatar="/src/Mask group.png">
+    Hello world
+    </Tweet>
+    </div>
+  </>
+    
+  )
+}
+
