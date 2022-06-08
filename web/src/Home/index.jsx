e077d0eb15ef79b17a359ad1493bef2e4ebb73c8
Matheus Affonso <matheus.loubach@gmail.com>
2022-06-08 15:52:50 -0300

    front  atualização

--- web/src/Home/index.jsx
+++ web/src/Home/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import { HeartIcon } from '@heroicons/react/outline'
 import { useFormik} from 'formik'
 import axios from 'axios'
+import avatar from './avatar.png'
 
 const MAX_TWEET_CHAR = 140
 
@@ -34,6 +35,7 @@ function TweetForm({ loggedInUser, onSuccess }) {
   return (
     <div className="border-b border-silver p-4 space-y-6">
       <div className="flex space-x-5">
+        <img src={avatar} className="w-7" />
         <h1 className="font-bold text-xl">Página Inicial</h1>
       </div>
 
@@ -69,7 +71,7 @@ function Tweet({ name, username, avatar, children }) {
   return (
     <div className="flex space-x-3 p-4 border-b border-silver">
         <div>
-  
+          <img src={avatar} />
         </div>
         <div className="space-y-1">
             <span className="font-bold text-sm">{name}</span>{' '}
@@ -108,7 +110,7 @@ export function Home({ loggedInUser }) {
       <TweetForm loggedInUser={loggedInUser} onSuccess={getData} />
       <div>
         {data.length && data.map(tweet => (
-          <Tweet key={tweet.id} name={tweet.user.name} username={tweet.user.username} >
+          <Tweet key={tweet.id} name={tweet.user.name} username={tweet.user.username} avatar={avatar}>
             {tweet.text}
           </Tweet>
         ))}
